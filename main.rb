1e148e41b0fbf93291b2752d5f7363fb32663e65
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-10 16:25:31 -0500

    exercises

--- main.rb
+++ main.rb
@@ -3,9 +3,21 @@ require_relative 'patient'
 new_patient = Patient.new("Jisie", "David")
 puts new_patient.first_name
 puts new_patient.last_name
+begin
 puts patient_contact_info
+rescue => e
+	puts "This variable has not been declared"
+end
 puts new_patient.patient_status(:coma)
+
+begin
 puts new_patient.patient_currently_admitted?
+rescue => e
+	puts "You must pass an argument to .patient_currently_admitted?"
+
+end
+
 puts new_patient.patient_new_total(125)
-new_patient.add_med_to_list("lipitor", 123.12)
+
+new_patient.add_med_to_list("Lipitor", 123.12)
 new_patient.patient_medications_list
--- patient.rb
+++ patient.rb
@@ -1,3 +1,12 @@
+#Create a new class called MedTypeError that inherits from StandardError and use that as seen below:
+
+class MedTypeError < StandardError
+  # def initialize(message)
+  #   super(message)
+  # end
+end
+
+
 class Patient
     attr_accessor :first_name, :last_name, :status, :current_balance, :med_list
 
@@ -7,21 +16,17 @@ class Patient
     @med_list = []
   end
 
-  begin
-    rescue NameError => e
-      puts "No name was given"
-      puts "Exception message: #{e.message}"
-      puts "Exception backtrace #{e.backtrace}"
-
-  end
-
   def patient_status(status)
-    raise ArgumentError,"Argument is not a string" unless age.is_a? String
+    raise ArgumentError,"Argument is not a string" unless status.is_a? String
 
-    @status = status
+    rescue
+      @status = status
   end
 
   def patient_currently_admitted?(admit_time)
+
+    raise ArgumentError, "Put in one admit time" unless ARGV.length == 1
+
     puts "Patient currently in hospital, admitted at #{admit_time}"
   end
 
@@ -30,30 +35,49 @@ class Patient
     puts "Current balance before calculation: #{current_balance}"
 
     @current_balance = (current_balance + amount)/current_balance
-
+    rescue ZeroDivisionError => e
+      puts "current_balance cannot be 0"
     # This runs only if there are no exceptions
-    @current_balance = amount
+    else
+      @current_balance = amount
   end
 
   def add_med_to_list(med, dosage)
-    raise ArgumentError,"#{med} is not a string" unless med.is_a? String
+
+    begin
+
+    raise MedTypeError,"#{med} is not a string" unless med.is_a? String
 
     raise ArgumentError,"#{dosage} is not a integer" unless dosage.is_a? Integer
+
+    rescue ArgumentError => e
+      puts "dosage must be an integer"
 
     puts "Medication added: #{med}"
     @med_list.push([med,dosage])
 
-    #Create a new class called MedTypeError that inherits from StandardError and use that as seen below:
-    raise MedTypeError, "This is not a valid medication"
-    puts @med_list
+    raise MedTypeError, "This is not a valid medication" unless med.equal? "Ritalin"
+
+    rescue MedTypeError => e
+      puts "There has been a medication error"
+
+    else
+      puts @med_list
+
+    ensure
+      puts "Let's keep going"
+
+    end
+
   end
 
   def patient_medications_list
     puts "User medication list:"
 
-    raise RuntimeError, "Medications list is not valid."
+    raise RuntimeError, "Medications list is not valid." unless @med_list.length.equal? 1
     puts @med_list
 
+    # This needs to run whether there was an exception or not.
     ensure
       puts "Disclaimer: The hospital is not responsible for the user's health due to medications they may be using."
   end
